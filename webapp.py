73a8d718888aaec935389789859be20b1ea57df9
Thomas Graft <mr@thomasgraft.com>
2011-10-06 15:44:26 -0400

    added API

--- webapp.py
+++ webapp.py
@@ -1,31 +1,45 @@
 from gifexplode import GifExplode
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, jsonify
 
-DEBUG=False
+DEBUG=True
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    
     if request.form:
-        return process()
+        
+        image_url = request.form['image_url']
+        frames = _explode_image_url(image_url)
+                                
+        return render_template('index.html', 
+                               frames=frames,
+                               image_url=image_url)
+                                          
     else:
         return render_template('index.html')
+                                          
+    
+@app.route("/api/explode", methods=['GET'])
+def api_explode():
+    
+    image_url = request.args['image_url']
+    frames = _explode_image_url(image_url)
+    
+    return jsonify(frames=frames)
 
+def _explode_image_url(image_url):
     
-def process():
-    
-    image_url = request.form['image_url']
     try:
         exploder = GifExplode(image_url)
         frames = exploder.explode()
     except ValueError:
         abort(500)
-        
-    return render_template('index.html', frames=frames,
-                            image_url=image_url)
     
+    return frames
+
 
 if __name__ == "__main__":
     app.run(debug=DEBUG, host='0.0.0.0')
