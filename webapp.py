14f51c98a784cf536c693d492ad5b35f82d8194a
Thomas Graft <mr@thomasgraft.com>
2011-10-20 11:06:08 -0400

    testing memcached support on heroku

--- webapp.py
+++ webapp.py
@@ -1,9 +1,12 @@
 from gifexplode import GifExplode
 from flask import Flask, render_template, request, abort, jsonify
 
+from werkzeug.contrib.cache import MemcachedCache
+
 DEBUG=False
 
 app = Flask(__name__)
+cache = MemcachedCache(['mc6.ec2.northscale.net:11211'])
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -40,11 +43,19 @@ def api_explode():
 
 def _explode_image_url(image_url):
 
-    try:
-        exploder = GifExplode(image_url)
-        frames = exploder.explode()
-    except ValueError:
-        abort(500)
+    frames = cache.get(image_url)
+    
+    if frames is None:
+        print "Nothing found in cache for %s" % image_url
+        try:
+            exploder = GifExplode(image_url)
+            frames = exploder.explode()
+            print "Setting cache for %s" % image_url
+            cache.set(image_url, frames, timeout=5*60)
+        except ValueError:
+            abort(500)
+    else:
+        print "Using cache for %s" % image_url
     
     return frames
 
