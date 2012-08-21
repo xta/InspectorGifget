4fbfd58f847d6ad57e8085144e38bc975f97050c
Rex Feng <rexfeng@gmail.com>
2012-08-21 19:38:09 -0400

    update js to prevent gif out of sync

--- static/custom.js
+++ static/custom.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 	var count = photos.length;
 	var i = 0;
+  var j = 0;
+  var speed = 1000;
 
 (function(){
     $('#frameimg1').prepend('<img src="data:image/gif;base64,' + photos[0].image +  '" />');
@@ -9,12 +11,20 @@ $(document).ready(function() {
   
 (function(){
       setInterval(function(){
-          $('#frameimg2').children().eq(0).remove();
-          	i++;
-          	if (i == count) {	i = 0; }  
+          i++;
+          if (i == count) { i = 0; }  
+
+          j = i - 1;
+          if (j == -1) { j = count; }
+
+          $('#frameimg2').children().eq(0).remove();	
           $('#frameimg2').prepend('<img src="data:image/gif;base64,' + photos[i].image +  '" />');
           $('#currentCount').html(i+1);
-			},1000);
+
+          $('#frameimg1').children().eq(0).remove();
+          $('#frameimg1').prepend('<img src="data:image/gif;base64,' + photos[j].image +  '" />');
+          
+			},speed);
   })($(this));
 
   
